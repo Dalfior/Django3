586efcd9bddcccaf47e47f91439fbc4015d44ed3
Dalfior <aecio.dalfior@gmail.com>
2021-08-20 19:48:17 -0300

    Criação do app webseite.

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
-        'USER': 'detran',
-        'PASSWORD': 'd3tr4n',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
